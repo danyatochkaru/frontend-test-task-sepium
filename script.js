11d2688067e6ba7612c94bf8c10f3a775de5999d
Даня <annawer4589@gmail.com>
2026-03-03 17:42:41 +0300

    feat: like & variants switchable now

--- script.js
+++ script.js
@@ -1,27 +1,88 @@
 const mainEl = document.querySelector("main");
 const templateCard = document.querySelector("#card");
 const templateSlide = document.querySelector("#slide");
+const templateVariant = document.querySelector("#variant");
 
-const onClickCardHandler = (e) => {
-    if (e.target.tagName !== "A" && e.target.tagName !== "BUTTON") {
-        window.open('/card', "_blank");
+class Card {
+    constructor(template, sliders = [], variants = []) {
+        this.template = template
+        this.clone = this.#getClone(template)
+        this.sliders = sliders
+        this.variants = variants
+    }
+
+    onClickHandler(e) {
+        if (e.target.tagName !== "A" && e.target.tagName !== "BUTTON") {
+            window.open('/card', "_blank");
+        }
+    }
+
+    onClickLikeHandler(e) {
+        if (e.target.tagName === "BUTTON" && e.target.classList.contains('card_actions_like')) {
+            e.target.classList.toggle('card_actions_like--active');
+            const isActive = e.target.classList.contains('card_actions_like--active');
+            const count = +e.target.querySelector('span').textContent;
+            e.target.querySelector('span').textContent = isActive ? count + 1 : count - 1;
+        }
+    }
+
+    #getClone(template = this.template) {
+        return document.importNode(template.content, true);
+    }
+
+    #loadVariants() {
+        const varsList = this.clone.querySelector('.card_vars_list');
+
+        for (const index in this.variants) {
+            const variantClone = this.#getClone(templateVariant);
+            variantClone.querySelector('.card_vars_item span').textContent = this.variants[index];
+
+            if (+index === 0) {
+                variantClone.querySelector('.card_vars_item').classList.add('card_vars_item--active');
+            }
+
+            varsList.appendChild(variantClone)
+        }
+
+        varsList.querySelectorAll('li button').forEach(i =>
+            i.addEventListener("click", (e) => {
+                varsList.querySelector('.card_vars_item--active').classList.remove('card_vars_item--active');
+                e.target.classList.add('card_vars_item--active');
+            })
+        )
+    }
+
+    #loadSliders() {
+        for (const slide of this.sliders) {
+            const cloneSlide = this.#getClone(templateSlide);
+            cloneSlide.querySelector('img').src = slide;
+            cloneSlide.querySelector('a').href = slide;
+
+            this.clone.querySelector('.splide__list').appendChild(cloneSlide);
+        }
+    }
+
+    init() {
+        if (!this.template) {
+            throw new Error("Template not found");
+        }
+
+        this.#loadSliders();
+        this.#loadVariants();
+
+        mainEl.appendChild(this.clone);
+        mainEl.lastElementChild.addEventListener("click", this.onClickHandler);
+        mainEl.lastElementChild.querySelector('.card_actions_like').addEventListener("click", this.onClickLikeHandler);
     }
 }
 
 if ("content" in document.createElement("template")) {
-    for (let i = 0; i < 5; i++) {
-        const clone = templateSlide.content.cloneNode(true);
-
-        templateCard.content.querySelector('.splide__list').appendChild(clone);
-    }
-
     for (let i = 0; i < 6; i++) {
-        const clone = templateCard.content.cloneNode(true);
-
-        mainEl.appendChild(clone);
+        const card = new Card(
+            templateCard,
+            ['assets/slide.png', 'assets/slide.png', 'assets/slide.png', 'assets/slide.png', 'assets/slide.png', 'assets/slide.png'],
+            ['ПВХ', 'Эмаль', 'Пластик', 'Массив'],
+        );
+        card.init();
     }
-
-    mainEl.childNodes.forEach((el) => {
-        el.addEventListener("click", onClickCardHandler);
-    });
 }
